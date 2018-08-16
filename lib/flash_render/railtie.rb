a4de94a96f3d533852e7db4b50a357dc660a2f3a
Tanel Suurhans <tanel@suurhans.com>
2018-08-15 23:21:38 -0400

    Update for Rails 5 support

--- lib/flash_render/railtie.rb
+++ lib/flash_render/railtie.rb
@@ -1,10 +1,10 @@
-require 'flash_render'
-require 'rails'
+require "flash_render"
+require "rails"
 
 module FlashRender
   class Railtie < ::Rails::Railtie
     initializer "flash_render.add_to_action_controller" do
-      ActionController::Base.send :include, FlashRender
+      ActionController::Base.include(FlashRender)
     end
   end
 end
